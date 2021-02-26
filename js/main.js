d765dad0056ebde9026735953dbd471267eb8d7f
abe menashy <abestbet1@gmail.com>
2021-02-26 12:50:56 -0800

    slider

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let pageViews = document.querySelector('.pageviews');
+let progress = document.querySelector('.progress-overlay');
 
 // pageViews.innerHTML = 'hello';
 
@@ -11,22 +12,27 @@ slider.oninput = function () {
     case '1':
       pageViews.innerHTML = '10K pageviews ';
       monthly.innerHTML = '$8 per month';
+      progress.style = 'width: 0';
       break;
     case '2':
       pageViews.innerHTML = '50K pageviews';
       monthly.innerHTML = '$12 per month';
+      progress.style = 'width: 25%';
       break;
     case '3':
       pageViews.innerHTML = '100K pageviews';
       monthly.innerHTML = '$16 per month';
+      progress.style = 'width: 50%';
       break;
     case '4':
       pageViews.innerHTML = '500k pageviews';
       monthly.innerHTML = '$24 per month';
+      progress.style = 'width: 75%';
       break;
     case '5':
       pageViews.innerHTML = '1M pageviews';
       monthly.innerHTML = '$36 per month';
+      progress.style = 'width: 100%';
       break;
     default:
       console.log(typeof this.value);
